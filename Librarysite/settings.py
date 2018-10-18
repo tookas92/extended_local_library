4f3b2a2edad0c425e8a718af21b370de08d60d24
tookas92 <pzawadzki92@gmail.com>
2018-10-18 23:23:38 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- Librarysite/settings.py
+++ Librarysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xy52n@(eln%k*(2dz8an%@bw1+p*sk
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salty-lake-29933.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
